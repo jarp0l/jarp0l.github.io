5d72381f22fcad2ce2c9d41c112b57b4746ea8d3
Prajwol Pradhan <57973356+jarp0l@users.noreply.github.com>
2025-03-16 15:27:56 +0545

    ci: make config changes & formatting to fix error in CI

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,23 @@
-import { createMDX } from 'fumadocs-mdx/next';
+import { createMDX } from "fumadocs-mdx/next";
 
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  
+  // Configure the output directory
+  distDir: "out",
+
+  // Configure a base path, esp. for GitHub Pages
+  // https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+  basePath: process.env.PAGES_BASE_PATH,
+
+  // Disable server side image optimization
+  // https://nextjs.org/docs/api-reference/next/image#unoptimized
+  // images: {
+  //   unoptimized: true,
+  // }
 };
 
 export default withMDX(config);
